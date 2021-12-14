e22dd836304031a1903728c56c73ea5f75172a1c
KalinHar <kalix@abv.bg>
2021-12-14 09:21:39 +0200

    add libs

--- src/lib.js
+++ src/lib.js
@@ -1,5 +1,8 @@
-import {html, render} from 'https://unpkg.com/lit-html?module';
-import page from 'https://unpkg.com/page/page.mjs';
+import {html, render} from './lib/lit-html.js';
+import page from './lib/page.mjs';
+
+// import {html, render} from 'https://unpkg.com/lit-html?module';
+// import page from 'https://unpkg.com/page/page.mjs';
 
 export {
     html,
